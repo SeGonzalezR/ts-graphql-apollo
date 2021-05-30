1f8ec012f15624fcb11b17270dc51c98985b8135
Sebastián González Riffo <sebastian@gonzalezr.cl>
2021-05-30 15:25:41 -0400

    fix(sentry): corregimos implementacion sentry
    ```ts
    import * as Sentry from '@sentry/node'
    ```

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -1,16 +1,15 @@
 import { env } from './enviroments'
-import Sentry from '@sentry/node'
+import * as Sentry from '@sentry/node'
 
 /**
  * Configuración Sentry.
  */
 if (typeof env.SentryConfig !== 'undefined') {
   Sentry.init({
-    dsn: env.SentryConfig.SENTRY_DSN,
-    release: env.SentryConfig.SENTRY_RELEASE || 'package-release',
-    serverName: env.SentryConfig.SENTRY_NAME,
-    environment: env.SentryConfig.SENTRY_ENVIRONMENT || env.enviroment
+    dsn: env.SentryConfig?.SENTRY_DSN,
+    release: env.SentryConfig?.SENTRY_RELEASE || 'package-release',
+    serverName: env.SentryConfig?.SENTRY_NAME,
+    environment: env.SentryConfig?.SENTRY_ENVIRONMENT || env.enviroment
   })
 }
-
 export default Sentry
